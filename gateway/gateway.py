273afb6ae7180aed6c3e3d0e5105f7d8c0269a52
DavidKendall <david.kendall@northumbria.ac.uk>
2016-03-05 16:35:13 +0000

    Minor improvements in style

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -6,67 +6,70 @@ from autobahn.twisted.websocket import WebSocketClientFactory
 from json import dumps, loads
 from mkxbeetx import xbeeMkPacketFromString, xbeeChecksum
 
+
 class WSProtocol(WebSocketClientProtocol):
 
-   def onConnect(self, response):
-      print("Server connected: {0}".format(response.peer))
+  def onConnect(self, response):
+    print("Server connected: {0}".format(response.peer))
 
-   def onOpen(self):
-      print("WebSocket connection open.")
-      self.sp = SerialPort(factory.sNodeProtocol(self), '/dev/ttyUSB0', reactor, baudrate='115200')
+  def onOpen(self):
+    print("WebSocket connection open.")
+    self.sp = SerialPort(factory.sNodeProtocol(
+        self), '/dev/ttyUSB0', reactor, baudrate='115200')
 
-   def onMessage(self, payload, isBinary):
-      if isBinary:
-         print("Binary message received: {0}".format(int(payload)))
-      else:
-         data = loads(payload.decode('utf8'))
-         if data[u'type'] == u'COMMAND':
-           sensorId = data[u'id']
-           print(repr(self.sp.protocol.nodes[sensorId]))
-           longAddr = self.sp.protocol.nodes[sensorId]['longAddr']
-           shortAddr = self.sp.protocol.nodes[sensorId]['shortAddr']
-           if data[u'to'] == u'LED1_TOGGLE':
-             packet = xbeeMkPacketFromString('1:1,', longAddr, shortAddr)
-             print(["%02x" % ord(c) for c in packet])
-             self.sp.protocol.transport.write(packet)
-           elif data[u'to'] == u'LED2_TOGGLE':
-             packet = xbeeMkPacketFromString('1:2,', longAddr, shortAddr)
-             self.sp.protocol.transport.write(packet)
-           elif data[u'to'] == u'LED3_TOGGLE':
-             packet = xbeeMkPacketFromString('1:3,', longAddr, shortAddr)
-             self.sp.protocol.transport.write(packet)
-           elif data[u'to'] == u'LED4_TOGGLE':
-             packet = xbeeMkPacketFromString('1:4,', longAddr, shortAddr)
-             self.sp.protocol.transport.write(packet)
-           elif data[u'to'] == u'SMOOTH_TOGGLE':
-             packet = xbeeMkPacketFromString('1:5,', longAddr, shortAddr)
-             self.sp.protocol.transport.write(packet)
-           elif data[u'to'] == u'TEST_MESSAGE':
-             packet = xbeeMkPacketFromString('1:6,', longAddr, shortAddr)
-             self.sp.protocol.transport.write(packet)
-           else:
-             pass
-           print(data)
+  def onMessage(self, payload, isBinary):
+    if isBinary:
+      print("Binary message received: {0}".format(int(payload)))
+    else:
+      data = loads(payload.decode('utf8'))
+      if data[u'type'] == u'COMMAND':
+        sensorId = data[u'id']
+        print(repr(self.sp.protocol.nodes[sensorId]))
+        longAddr = self.sp.protocol.nodes[sensorId]['longAddr']
+        shortAddr = self.sp.protocol.nodes[sensorId]['shortAddr']
+        if data[u'to'] == u'LED1_TOGGLE':
+          packet = xbeeMkPacketFromString('1:1,', longAddr, shortAddr)
+          print(["%02x" % ord(c) for c in packet])
+          self.sp.protocol.transport.write(packet)
+        elif data[u'to'] == u'LED2_TOGGLE':
+          packet = xbeeMkPacketFromString('1:2,', longAddr, shortAddr)
+          self.sp.protocol.transport.write(packet)
+        elif data[u'to'] == u'LED3_TOGGLE':
+          packet = xbeeMkPacketFromString('1:3,', longAddr, shortAddr)
+          self.sp.protocol.transport.write(packet)
+        elif data[u'to'] == u'LED4_TOGGLE':
+          packet = xbeeMkPacketFromString('1:4,', longAddr, shortAddr)
+          self.sp.protocol.transport.write(packet)
+        elif data[u'to'] == u'SMOOTH_TOGGLE':
+          packet = xbeeMkPacketFromString('1:5,', longAddr, shortAddr)
+          self.sp.protocol.transport.write(packet)
+        elif data[u'to'] == u'TEST_MESSAGE':
+          packet = xbeeMkPacketFromString('1:6,', longAddr, shortAddr)
+          self.sp.protocol.transport.write(packet)
+        else:
+          pass
+        print(data)
 
-   def onClose(self, wasClean, code, reason):
-      print("WebSocket connection closed: {0}".format(reason))
+  def onClose(self, wasClean, code, reason):
+    print("WebSocket connection closed: {0}".format(reason))
 
 
-GET_START         = 0
-GET_LEN           = 1
-GET_FRAME_TYPE    = 2
+GET_START = 0
+GET_LEN = 1
+GET_FRAME_TYPE = 2
 GET_REST_OF_FRAME = 3
-START_BYTE        = b'\x7E'
+START_BYTE = b'\x7E'
 
 
 class SNodeProtocol(Protocol):
-  
+
   def __init__(self, websocket):
     self.state = GET_START
     self.buffer = bytearray()
     self.length = 0
     self.nodes = dict()
     self.ws = websocket
+    self.badPacketCount = 0
 
   def cleanup(self):
     print("\nAttempting to close websocket")
@@ -78,7 +81,7 @@ class SNodeProtocol(Protocol):
     # self.sendLine('S')
 
   def dataReceived(self, data):
-    self.buffer += data     
+    self.buffer += data
     if self.state == GET_START:
       startPos = self.buffer.find(START_BYTE)
       if startPos >= 0:
@@ -89,7 +92,12 @@ class SNodeProtocol(Protocol):
       if len(self.buffer) >= 3:
         self.length = 256 * self.buffer[1] + self.buffer[2] + 3
         print('Length : {:d}'.format(self.length))
-        self.state = GET_FRAME_TYPE
+        if 0 <= self.length and self.length <= 72:
+          self.state = GET_FRAME_TYPE
+        else:
+          self.badPacketCount += 1
+          self.buffer = bytearray()
+          self.state = GET_START
     elif self.state == GET_FRAME_TYPE:
       self.frameType = self.buffer[3]
       print("%02x" % self.frameType)
@@ -97,37 +105,42 @@ class SNodeProtocol(Protocol):
     elif self.state == GET_REST_OF_FRAME:
       if len(self.buffer) >= self.length:
         packet = self.buffer[:self.length]
-        print("Packet: "), 
+        print("Packet: "),
         print(repr(packet))
         self.buffer = self.buffer[self.length:]
         print("Buffer: "),
         print(repr(self.buffer))
-        if ((self.frameType == 0x90)  or (self.frameType == 0x91)):
-          startAppData = 32 if self.frameType == '\x90' else 38
-          print("Data starts at {:d}".format(packet.find("type:DATA")))
-          try:
-            sensorData = dict([s.split(':') for s in str(packet[startAppData:self.length-1]).split(',')])
-            sensorId = sensorData.get('id')
-            if sensorId != None:
-              msg = dumps(sensorData)
-              print(msg)
-              self.ws.sendMessage(msg)
-              if sensorId not in self.nodes:
-                self.nodes[sensorId] = {'longAddr' : packet[4:12], 'shortAddr' : packet[12:14]}
-                print(repr(self.nodes))
-          except ValueError:
-            print("Dictionary error: " + repr(packet)) 
+        if ((self.frameType == 0x90) or (self.frameType == 0x91)):
+          startAppData = packet.find("type:DATA")
+          if startAppData > 0:
+            print("Data starts at {:d}".format(startAppData))
+            try:
+              sensorData = dict([s.split(':') for s in str(
+                  packet[startAppData:self.length - 1]).split(',')])
+              sensorId = sensorData.get('id')
+              if sensorId != None:
+                msg = dumps(sensorData)
+                print(msg)
+                self.ws.sendMessage(msg)
+                if sensorId not in self.nodes:
+                  self.nodes[sensorId] = {'longAddr': packet[
+                      4:12], 'shortAddr': packet[12:14]}
+                  print(repr(self.nodes))
+            except ValueError:
+              print("Dictionary error: " + repr(packet))
         else:
           print("Bad packet: " + repr(packet))
+          self.badPacketCount += 1
         self.state = GET_START
     else:
       print("UNKNOWN STATE")
+    print("Bad Packet Count : {:d}\n".format(self.badPacketCount))
 
 
 if __name__ == '__main__':
-   factory = WebSocketClientFactory("ws://www.hesabu.net:9000/ws/sensors/rw", 
-                                    debug = False)
-   factory.protocol = WSProtocol
-   factory.sNodeProtocol = SNodeProtocol
-   reactor.connectTCP("www.hesabu.net", 9000, factory)
-   reactor.run()
+  factory = WebSocketClientFactory("ws://localhost:9000/ws/sensors/rw",
+                                   debug=False)
+  factory.protocol = WSProtocol
+  factory.sNodeProtocol = SNodeProtocol
+  reactor.connectTCP("localhost", 9000, factory)
+  reactor.run()
--- www/lpc4088/js/websocket.js
+++ www/lpc4088/js/websocket.js
@@ -1,106 +1,178 @@
-/*jslint browser: true */
+/*jslint browser: true*/
 /*global jQuery, SmoothieChart, TimeSeries, manipulateDOM, WebSocket, $*/
 
-var WSN_CLIENT = (function () {
-    'use strict';
+var WSN_CLIENT = (function() {
+  'use strict';
 
-    var activityAcc = 0,
-        accX = new TimeSeries(),
-        accY = new TimeSeries(),
-        accZ = new TimeSeries(),
-        dataAccX = 0,
-        dataAccY = 0,
-        dataAccZ = 0,
-        websocket,
-        chartAcc;
+  var activityAcc = 0;
+  var accX = new TimeSeries();
+  var accY = new TimeSeries();
+  var accZ = new TimeSeries();
+  var dataAccX = 0;
+  var dataAccY = 0;
+  var dataAccZ = 0;
+  var websocket;
+  var chartAcc;
 
-    function showGraph(chart) {
-        if (chart === "acc") {
-            $('#chartarea_acc').slideDown();
-            document.getElementById("chart_acc").width = 300;
-            document.getElementById("chart_acc").height = 200;
-        }
+  function showGraph(chart) {
+    if (chart === 'acc') {
+      $('#chartarea_acc').slideDown();
+      document.getElementById('chart_acc').width = 300;
+      document.getElementById('chart_acc').height = 200;
     }
+  }
 
-    function showMain() {
-        showGraph("acc");
-    }
+  function showMain() {
+    showGraph('acc');
+  }
 
-    function createTimeline() {
-        chartAcc = new SmoothieChart({strokeStyle: 'rgb(125, 0, 0)', fillStyle: 'rgb(60, 0, 0)', lineWidth: 1, maxValue: 120, minValue: -120, millisPerLine: 250, verticalSections: 6 });
-        chartAcc.addTimeSeries(accX, { strokeStyle: 'rgba(255, 0, 0, 1)', fillStyle: 'rgba(255, 0, 0, 0.1)', lineWidth: 3 });
-        chartAcc.addTimeSeries(accY, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.1)', lineWidth: 3 });
-        chartAcc.addTimeSeries(accZ, { strokeStyle: 'rgba(0, 0, 255, 1)', fillStyle: 'rgba(0, 0, 255, 0.1)', lineWidth: 3 });
-        chartAcc.streamTo(document.getElementById("chart_acc"), 200);
-    }
-
-    function isActiveAcc() {
-        if (activityAcc === 0) {
-            $("#chart_acc").fadeTo(10, 0.3);
-        }
-        activityAcc = 0;
-        setTimeout(isActiveAcc, 2000);
-    }
-
-    $(document).ready(function () {
-        showMain();
-        createTimeline();
-
-        websocket = new WebSocket('ws://hesabu.net:9000/ws/sensors/rw');
-
-        websocket.onopen = function () {
-            var skip = 0;
-            skip = skip + 1;
-        };
-
-        websocket.onmessage = function (evt) {
-            var theDate,
-                jsonSensor = jQuery.parseJSON(evt.data.toString());
-            theDate = new Date().getTime();
-            if (jsonSensor.id === "SN01") {
-                activityAcc = 1;
-                $("#chart_acc").fadeTo(100, 1);
-                dataAccX = jsonSensor.ax;
-                dataAccY = jsonSensor.ay;
-                dataAccZ = jsonSensor.az;
-
-                accX.append(theDate, dataAccX);
-                accY.append(theDate, dataAccY);
-                accZ.append(theDate, dataAccZ);
-            } 
-        };
-
-        websocket.onclose = function () {
-            var skip = 0;
-            skip = skip + 1;
-        };
-
-        isActiveAcc();
+  function createTimeline() {
+    chartAcc = new SmoothieChart({
+      strokeStyle: 'rgb(125, 0, 0)',
+      fillStyle: 'rgb(60, 0, 0)',
+      lineWidth: 1,
+      maxValue: 120,
+      minValue: -120,
+      millisPerLine: 250,
+      verticalSections: 6,
     });
-    return {
-        led1Command : function () {
-            var data = JSON.stringify({id : 'SN01', type : 'COMMAND', to : 'LED1_TOGGLE'});
-            websocket.send(data);
-        },
-        led2Command : function () {
-            var data = JSON.stringify({id : 'SN01', type : 'COMMAND', to : 'LED2_TOGGLE'});
-            websocket.send(data);
-        },
-        led3Command : function () {
-            var data = JSON.stringify({id : 'SN01', type : 'COMMAND', to : 'LED3_TOGGLE'});
-            websocket.send(data);
-        },
-        led4Command : function () {
-            var data = JSON.stringify({id : 'SN01', type : 'COMMAND', to : 'LED4_TOGGLE'});
-            websocket.send(data);
-        },
-        smoothCommand : function () {
-            var data = JSON.stringify({id : 'SN01', type : 'COMMAND', to : 'SMOOTH_TOGGLE'});
-            websocket.send(data);
-        },
-        testMessageCommand : function () {
-            var data = JSON.stringify({id : 'SN01', type : 'COMMAND', to : 'TEST_MESSAGE'});
-            websocket.send(data);
-        }
+    chartAcc.addTimeSeries(accX, {
+      strokeStyle: 'rgba(255, 0, 0, 1)',
+      fillStyle: 'rgba(255, 0, 0, 0.1)',
+      lineWidth: 3,
+    });
+    chartAcc.addTimeSeries(accY, {
+      strokeStyle: 'rgba(0, 255, 0, 1)',
+      fillStyle: 'rgba(0, 255, 0, 0.1)',
+      lineWidth: 3,
+    });
+    chartAcc.addTimeSeries(accZ, {
+      strokeStyle: 'rgba(0, 0, 255, 1)',
+      fillStyle: 'rgba(0, 0, 255, 0.1)',
+      lineWidth: 3,
+    });
+    chartAcc.streamTo(document.getElementById('chart_acc'), 200);
+  }
+
+  function isActiveAcc() {
+    if (activityAcc === 0) {
+      $('#chart_acc').fadeTo(10, 0.3);
+    }
+
+    activityAcc = 0;
+    setTimeout(isActiveAcc, 2000);
+  }
+
+  function parseDocumentURL() {
+    // var url = document.getElementsByTagName('a')[0];
+    var url = document.URL;
+    var urlComponents = url.split('=');
+    var i;
+    for (i = 0; i < urlComponents.length; i += 1) {
+      console.log(urlComponents[i]);
+    }
+    // console.log(
+    //   'Doc URL  :' + document.URL + '\n' +
+    //   'URL      :' + url.href + '\n' + // the full URL
+    //   'Protocol :' + url.protocol + '\n' + // http:
+    //   'Hostname :' + url.hostname + '\n' + // site.com
+    //   'Port     :' + url.port + '\n' + // 81
+    //   'Pathname :' + url.pathname + '\n' + // /path/page
+    //   'Search   :' + url.search + '\n' + // ?a=1&b=2
+    //   'Hash     :' + url.hash // #hash
+    // );
+  }
+
+  $(document).ready(function() {
+    console.log('websocket.js running...');
+    parseDocumentURL();
+    showMain();
+    createTimeline();
+
+    websocket = new WebSocket('ws://localhost:9000/ws/sensors/rw');
+
+    websocket.onopen = function() {
+      var skip = 0;
+      skip = skip + 1;
     };
+
+    websocket.onmessage = function(evt) {
+      var jsonSensor = jQuery.parseJSON(evt.data.toString());
+      var theDate = new Date().getTime();
+      if (jsonSensor.id === 'SN01') {
+        activityAcc = 1;
+        $('#chart_acc').fadeTo(100, 1);
+        dataAccX = jsonSensor.ax;
+        dataAccY = jsonSensor.ay;
+        dataAccZ = jsonSensor.az;
+
+        accX.append(theDate, dataAccX);
+        accY.append(theDate, dataAccY);
+        accZ.append(theDate, dataAccZ);
+      }
+    };
+
+    websocket.onclose = function() {
+      var skip = 0;
+      skip = skip + 1;
+    };
+
+    isActiveAcc();
+  }
+  );
+  return {
+    led1Command: function() {
+      var data = JSON.stringify({
+        id: 'SN01',
+        type: 'COMMAND',
+        to: 'LED1_TOGGLE',
+      });
+      websocket.send(data);
+    },
+
+    led2Command: function() {
+      var data = JSON.stringify({
+        id: 'SN01',
+        type: 'COMMAND',
+        to: 'LED2_TOGGLE',
+      });
+      websocket.send(data);
+    },
+
+    led3Command: function() {
+      var data = JSON.stringify({
+        id: 'SN01',
+        type: 'COMMAND',
+        to: 'LED3_TOGGLE',
+      });
+      websocket.send(data);
+    },
+
+    led4Command: function() {
+      var data = JSON.stringify({
+        id: 'SN01',
+        type: 'COMMAND',
+        to: 'LED4_TOGGLE',
+      });
+      websocket.send(data);
+    },
+
+    smoothCommand: function() {
+      var data = JSON.stringify({
+        id: 'SN01',
+        type: 'COMMAND',
+        to: 'SMOOTH_TOGGLE',
+      });
+      websocket.send(data);
+    },
+
+    testMessageCommand: function() {
+      var data = JSON.stringify({
+        id: 'SN01',
+        type: 'COMMAND',
+        to: 'TEST_MESSAGE',
+      });
+      websocket.send(data);
+    },
+  };
 }());
